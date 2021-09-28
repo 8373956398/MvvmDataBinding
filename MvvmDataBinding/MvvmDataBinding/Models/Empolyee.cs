e4243307c53a8c2d2ed18c6e780f17a9a49cdd09
Praveen <Praveen@DESKTOP-DD11LNH>
2021-09-28 19:15:46 +0530

    Add project files.

---
+++ MvvmDataBinding/MvvmDataBinding/Models/Empolyee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvvmDataBinding.Models
+{
+   public class Empolyee
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ MvvmDataBinding/MvvmDataBinding/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using MvvmDataBinding.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MvvmDataBinding.ViewModels
+{
+    class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainPageViewModel()
+        {
+            EmpolyeeList = new ObservableCollection<Empolyee>();
+            EmpolyeeList.Add(new Empolyee { Name = "Praveen Singh", Address = "Delhi" });
+            EmpolyeeList.Add(new Empolyee { Name = "Abhinav Singh", Address = "Nodia" });
+            EmpolyeeList.Add(new Empolyee { Name = "Ashwini Singh", Address = "Greater Nodia" });
+            EmpolyeeList.Add(new Empolyee { Name = "Sourabh Singh", Address = "Vijay enclave" });
+            EmpolyeeList.Add(new Empolyee { Name = "Rajani Devi", Address = "jaipur" });
+            EmpolyeeList.Add(new Empolyee { Name = "Neelam Lingwal", Address = "Sagarpur" });
+
+        }
+
+        ObservableCollection<Empolyee> _employeeList;
+        public ObservableCollection<Empolyee> EmpolyeeList
+        {
+            get { return _employeeList; }
+            set
+            {
+                _employeeList = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
